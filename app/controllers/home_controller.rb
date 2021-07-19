4beea35ef8e59f90e78668eac62b03218bd037b0
wrmk <elmakan@gmail.com>
2021-07-19 20:36:54 +0300

    refactoring

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,27 +3,27 @@ class HomeController < ApplicationController
 
   require_relative "../models/dadata"
 
+  
 
   def index
-    @gender_hash = {"М" => "Мужской", "Ж" => "Женский"}
-    if current_user
-      @name = current_user.userinfo.name
-      @email = current_user.email
-      if current_user.userinfo.sex != nil
-        @sex = current_user.userinfo.sex
-        @confirmed = true
-      else
-        # @sex = Dadata.gender(current_user)
-        @sex = @gender_hash["М"]
-        @confirmed = false
-      end 
-    end
+    gender_hash
+    @name = current_user.userinfo.name
+    @email = current_user.email
+    if current_user.userinfo.sex != nil
+      @sex = current_user.userinfo.sex
+      @confirmed = true
+    else
+      # @sex = Dadata.gender(current_user)
+      @sex = "Ж"
+      @confirmed = false
+    end 
   end
 
 
   def update
+    gender_hash
     current_user.userinfo.update gender_params
-    @sex = current_user.userinfo.sex
+    @sex = gender_params[:sex]
     respond_to do |format|
       format.html {render "index"}
       format.js
@@ -32,6 +32,10 @@ class HomeController < ApplicationController
 
 
   private
+
+  def gender_hash
+    @gender_hash = {"М" => "Мужской", "Ж" => "Женский"}
+  end
 
   def gender_params
       params.permit(:sex)
