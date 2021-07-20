32ed17b0250ef1e873f82f1ad2e276bd9fe9d61a
wrmk <elmakan@gmail.com>
2021-07-20 21:10:31 +0300

    add tooltip when gender update from service

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,21 +4,23 @@ class HomeController < ApplicationController
   require_relative "../models/dadata"
 
   def index
-    gender_hash
+    userinfo
     @name = current_user.userinfo.name
     @email = current_user.email
+    
     if current_user.userinfo.sex != nil
       @sex = current_user.userinfo.sex
       @confirmed = true
     else
       # @sex = Dadata.gender(current_user.userinfo.name)
       @sex = "Ж"
+      current_user.userinfo.update(:sex_updated => DateTime.now)
       @confirmed = false
     end 
   end
 
   def update
-    gender_hash
+    userinfo
     current_user.userinfo.update gender_params
     @sex = current_user.userinfo.sex
     respond_to do |format|
@@ -30,8 +32,9 @@ class HomeController < ApplicationController
 
   private
 
-  def gender_hash
+  def userinfo
     @gender_hash = {"М" => "Мужской", "Ж" => "Женский"}
+    @update_date = current_user.userinfo.sex_updated
   end
   def gender_params
     params.permit(:sex)
